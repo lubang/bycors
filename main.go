e8613330d2c56f2f15b0f71c972c29b45e18d92f
lubang <lubang@lulab.net>
2018-08-26 15:11:17 +0900

    add write headers for CORS

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"reflect"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
@@ -20,6 +21,7 @@ func main() {
 	router := httprouter.New()
 
 	router.NotFound = http.FileServer(http.Dir("public"))
+	router.OPTIONS("/route", routeAllowed)
 	router.POST("/route", routeCors)
 	router.GET("/route", routeCors)
 	router.PUT("/route", routeCors)
@@ -28,18 +30,42 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
+func routeAllowed(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	keys := reflect.ValueOf(r.Header).MapKeys()
+	strkeys := make([]string, len(keys))
+	for i := 0; i < len(keys); i++ {
+		strkeys[i] = keys[i].String()
+	}
+	headres := strings.Join(strkeys, ",") + ", X-Naver-Client-Id,X-Naver-Client-Secret,X-TARGET-URL"
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Headers", headres)
+}
+
 func routeCors(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	keys := reflect.ValueOf(r.Header).MapKeys()
+	strkeys := make([]string, len(keys))
+	for i := 0; i < len(keys); i++ {
+		strkeys[i] = keys[i].String()
+	}
+	headres := strings.Join(strkeys, ",") + ", X-Naver-Client-Id,X-Naver-Client-Secret,X-TARGET-URL"
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Headers", headres)
+
 	hostname := r.Header.Get("X-TARGET-URL")
 
 	client := &http.Client{}
 	req, err := http.NewRequest(r.Method, hostname, r.Body)
 	if err != nil {
 		fmt.Fprint(w, err)
-		log.Fatalln("Fail to route CORS from `" +
+		log.Fatalln("Fail CORS from `" +
 			r.Host +
 			"` to `" +
 			hostname +
-			"`: " + err.Error())
+			"`: New Request - " + err.Error())
 		return
 	}
 
@@ -55,7 +81,7 @@ func routeCors(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 			r.Host +
 			"` to `" +
 			hostname +
-			"`: " + err.Error())
+			"`: Client Do - " + err.Error())
 		return
 	}
 	defer resp.Body.Close()
@@ -71,8 +97,7 @@ func routeCors(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 			r.Host +
 			"` to `" +
 			hostname +
-			"`: " + err.Error())
-		return
+			"`: RealAll - " + err.Error())
 	}
 
 	fmt.Fprint(w, string(data))
